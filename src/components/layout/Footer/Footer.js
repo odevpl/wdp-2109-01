68fef2f8094f23d8f10640eff6e2eb1edef2732e
Marcin Skowronek <marcinfoto35@op.pl>
2021-09-09 22:08:02 +0200

    Done first part of footer

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -11,90 +11,97 @@ import {
   faLinkedinIn,
   faPinterestP,
 } from '@fortawesome/free-brands-svg-icons';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 const Footer = ({ children }) => (
   <footer className={styles.root}>
+    {/* <Container className={styles.container} maxWidth='lg'> */}
     <div className={styles.footerMenu}>
       <div className='container'>
-        <div className='row'>
-          <div className='col'>
-            <div className={styles.menuWrapper}>
-              <h6>Information</h6>
-              <ul>
-                <li>
-                  <a href='#'>About us</a>
-                </li>
-                <li>
-                  <a href='#'>Policy</a>
-                </li>
-                <li>
-                  <a href='#'>Conditions</a>
-                </li>
-                <li>
-                  <a href='#'>Online support</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className='col'>
-            <div className={styles.menuWrapper}>
-              <h6>My account</h6>
-              <ul>
-                <li>
-                  <a href='#'>Login</a>
-                </li>
-                <li>
-                  <a href='#'>My cart</a>
-                </li>
-                <li>
-                  <a href='#'>Wishlist</a>
-                </li>
-                <li>
-                  <a href='#'>Checkout</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className='col'>
-            <div className={styles.menuWrapper}>
-              <h6>Information</h6>
-              <ul>
-                <li>
-                  <a href='#'>Specials</a>
-                </li>
-                <li>
-                  <a href='#'>New products</a>
-                </li>
-                <li>
-                  <a href='#'>Best Sellers</a>
-                </li>
-                <li>
-                  <a href='#'>Out Stores</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className='col'>
-            <div className={styles.menuWrapper}>
-              <h6>Orders</h6>
-              <ul>
-                <li>
-                  <a href='#'>Payment options</a>
-                </li>
-                <li>
-                  <a href='#'>Shipping and delivery</a>
-                </li>
-                <li>
-                  <a href='#'>Returns</a>
-                </li>
-                <li>
-                  <a href='#'>Shipping</a>
-                </li>
-              </ul>
-            </div>
-            <img src='./images/cards.png' alt='Supported credit cards' />
-          </div>
-        </div>
+        <Container className={styles.container}>
+          <Grid container spacing={3}>
+            <Grid item xs={6} md={3}>
+              <div className={styles.menuWrapper}>
+                <h6>Information</h6>
+                <ul>
+                  <li>
+                    <a href='#'>About us</a>
+                  </li>
+                  <li>
+                    <a href='#'>Policy</a>
+                  </li>
+                  <li>
+                    <a href='#'>Conditions</a>
+                  </li>
+                  <li>
+                    <a href='#'>Online support</a>
+                  </li>
+                </ul>
+              </div>
+            </Grid>
+            <Grid item xs={6} md={3}>
+              <div className={styles.menuWrapper}>
+                <h6>My account</h6>
+                <ul>
+                  <li>
+                    <a href='#'>Login</a>
+                  </li>
+                  <li>
+                    <a href='#'>My cart</a>
+                  </li>
+                  <li>
+                    <a href='#'>Wishlist</a>
+                  </li>
+                  <li>
+                    <a href='#'>Checkout</a>
+                  </li>
+                </ul>
+              </div>
+            </Grid>
+            <Grid item xs={6} md={3}>
+              <div className={styles.menuWrapper}>
+                <h6>Information</h6>
+                <ul>
+                  <li>
+                    <a href='#'>Specials</a>
+                  </li>
+                  <li>
+                    <a href='#'>New products</a>
+                  </li>
+                  <li>
+                    <a href='#'>Best Sellers</a>
+                  </li>
+                  <li>
+                    <a href='#'>Out Stores</a>
+                  </li>
+                </ul>
+              </div>
+            </Grid>
+            <Grid item xs={6} md={3}>
+              <div className={styles.menuWrapper}>
+                <h6>Orders</h6>
+                <ul>
+                  <li>
+                    <a href='#'>Payment options</a>
+                  </li>
+                  <li>
+                    <a href='#'>Shipping and delivery</a>
+                  </li>
+                  <li>
+                    <a href='#'>Returns</a>
+                  </li>
+                  <li>
+                    <a href='#'>Shipping</a>
+                  </li>
+                </ul>
+              </div>
+            </Grid>
+            <Grid item xs={12} md={12} lg={12} className={styles.images}>
+              <img src='./images/cards.png' alt='Supported credit cards' />
+            </Grid>
+          </Grid>
+        </Container>
       </div>
     </div>
     <div className={styles.bottomBar}>
@@ -141,6 +148,7 @@ const Footer = ({ children }) => (
         </div>
       </div>
     </div>
+    {/* </Container> */}
   </footer>
 );
 
