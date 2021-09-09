19991ac276734c014128956b0e5078a14a0a7082
Agnieszka Falandys <agnfal@gmail.com>
2021-09-09 15:41:01 +0200

    Task WDP210901-11 - Favoritue products

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -11,7 +11,16 @@ import {
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
 
-const ProductBox = ({ name, price, promo, stars, oldPrice }) => (
+const ProductBox = ({
+  id,
+  name,
+  price,
+  promo,
+  stars,
+  oldPrice,
+  favoritue,
+  addToFavoritue,
+}) => (
   <div className={styles.root}>
     <div className={styles.photo}>
       {promo && <div className={styles.sale}>{promo}</div>}
@@ -39,7 +48,13 @@ const ProductBox = ({ name, price, promo, stars, oldPrice }) => (
     <div className={styles.line}></div>
     <div className={styles.actions}>
       <div className={styles.outlines}>
-        <Button variant='outline'>
+        <Button
+          variant={favoritue ? 'favoritue' : 'outline'}
+          onClick={event => {
+            event.preventDefault();
+            return addToFavoritue(id);
+          }}
+        >
           <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
         </Button>
         <Button variant='outline'>
@@ -57,12 +72,15 @@ const ProductBox = ({ name, price, promo, stars, oldPrice }) => (
 );
 
 ProductBox.propTypes = {
+  id: PropTypes.string,
   children: PropTypes.node,
   name: PropTypes.string,
   price: PropTypes.number,
   oldPrice: PropTypes.number,
   promo: PropTypes.string,
   stars: PropTypes.number,
+  favoritue: PropTypes.bool,
+  addToFavoritue: PropTypes.func,
 };
 
 export default ProductBox;
--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,9 +5,27 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+/* action name creator */
+const reducerName = 'products';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+/* action types */
+export const ADD_TO_FAVORITUE = createActionName('ADD_TO_FAVORITUE');
+
+/* action creators */
+export const addToFavoritue = payload => ({ payload, type: ADD_TO_FAVORITUE });
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case ADD_TO_FAVORITUE: {
+      const id = action.payload;
+      return statePart.map(product =>
+        product.id == id
+          ? { ...product, favoritue: !product.favoritue }
+          : { ...product }
+      );
+    }
     default:
       return statePart;
   }
