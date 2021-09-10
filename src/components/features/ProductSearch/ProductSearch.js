97b790767080f3d1e86044fc72f6f723765fb72d
Kai Mareving <karska.joanna@gmail.com>
2021-09-10 17:00:30 +0200

    ProductBox:Fix minor code error,ProductSearch:list styling,create container,use props[not working]

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -6,26 +6,48 @@ import { faListUl, faSearch, faCaretDown } from '@fortawesome/free-solid-svg-ico
 
 import styles from './ProductSearch.module.scss';
 
-const ProductSearch = () => (
-  <form action='' className={styles.root}>
-    <div className={styles.category}>
-      <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      <select name='' id=''>
-        <option value=''>Select a category</option>
-      </select>
-      <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-    </div>
-    <div className={styles.searchField}>
-      <input placeholder='Search products...' type='text' />
-      <button>
-        <FontAwesomeIcon className={styles.icon} icon={faSearch} />
-      </button>
-    </div>
-  </form>
-);
+const ProductSearch = ({ categories }) => {
+  console.log('categories:', categories);
+
+  return (
+    <form action='' className={styles.root}>
+      <div className={styles.category}>
+        <FontAwesomeIcon className={styles.icon} icon={faListUl} />
+        <div className='category-list'>
+          <ul>
+            Select a category
+            <li>One Bed</li>
+            <li>Three Sofas</li>
+            <li>Five Chairs</li>
+            {categories.map(item => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
+        </div>
+        <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+      </div>
+      <div className={styles.searchField}>
+        <input placeholder='Search products...' type='text' />
+        <button>
+          <FontAwesomeIcon className={styles.icon} icon={faSearch} />
+        </button>
+      </div>
+    </form>
+  );
+};
 
 ProductSearch.propTypes = {
   children: PropTypes.node,
+  categories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+    })
+  ),
+};
+
+ProductSearch.defaultProps = {
+  categories: [],
 };
 
 export default ProductSearch;
