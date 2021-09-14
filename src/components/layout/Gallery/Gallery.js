5593a8ca50d57cb251897d494b00cb9477829c6a
Kamila Kowalczyk-Radomska <kamila_kodilla@o2.pl>
2021-09-14 09:07:14 +0200

    Add component Gallery problems with photos

---
+++ src/components/layout/Gallery/Gallery.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Gallery.module.scss';
+import Button from './../../common/Button/Button';
+import ProductSlider from '../../common/ProductSlider/ProductSliderContainer';
+
+class Gallery extends React.Component {
+  render() {
+    return (
+      <section className={styles.root}>
+        <div className='container'>
+          <div className={'row ${styles.wrapper}'}>
+            <div className={'col-6 ${styles.leftWrapper}'}>
+              <ProductSlider />
+            </div>
+
+            <div className={'col-6 ${styles.rightWrapper}'}>
+              <div className={styles.imageWrapper}>
+                <img src='../../images/imagesGallery/chair-1.jpg' alt='' />
+                <div className={styles.priceTitle}>
+                  FROM<span>$50.80</span>
+                </div>
+                <div className={styles.priceSubtitle}>Bedroom bed</div>
+                <div className={styles.greenButton}>
+                  <Button variant='bigGreenButton'>SHOP NOW</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+Gallery.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      category: PropTypes.string,
+      price: PropTypes.number,
+      stars: PropTypes.number,
+      promo: PropTypes.string,
+      newFurniture: PropTypes.bool,
+      image: PropTypes.string,
+    })
+  ),
+};
+
+Gallery.defaultProps = {
+  products: [],
+};
+
+export default Gallery;
--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -2,9 +2,12 @@ import React from 'react';
 import styles from './Homepage.module.scss';
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
+
 import Feedback from '../../features/Feedback/FeedbackContainer';
 import ProductCompare from '../../features/ProductCompare/ProductCompareContainer';
 import Promoted from '../../layout/Promoted/Promoted';
+import Gallery from '../../layout/Gallery/GalleryContainer';
+
 
 const Homepage = () => (
   <div className={styles.root}>
@@ -13,6 +16,9 @@ const Homepage = () => (
     <NewFurniture />
     <Feedback />
     <ProductCompare />
+
+    <Gallery />
+
   </div>
 );
 
