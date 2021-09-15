0a9b4edcffc93d33ef7776754df7df858ad00309
Kamila Kowalczyk-Radomska <kamila_kodilla@o2.pl>
2021-09-15 15:02:10 +0200

    Add styles for Gallery

--- src/components/common/ProductSliderBox/ProductSliderBox.js
+++ src/components/common/ProductSliderBox/ProductSliderBox.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './ProductImgBoxSlider.module.scss';
+import styles from './ProductSliderBox.module.scss';
 
-const ProductImgBoxSlider = ({ name, image }) => {
+const ProductSliderBox = ({ name, image }) => {
   return (
     <div className={styles.imageShadow}>
       <img src={image} alt={name} />
@@ -10,9 +10,9 @@ const ProductImgBoxSlider = ({ name, image }) => {
   );
 };
 
-ProductImgBoxSlider.propTypes = {
+ProductSliderBox.propTypes = {
   name: PropTypes.string,
   image: PropTypes.string,
 };
 
-export default ProductImgBoxSlider;
+export default ProductSliderBox;
