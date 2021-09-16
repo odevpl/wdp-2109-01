2a8516c861c576c8f36a3993e6010604b00193aa
Agnieszka Falandys <agnfal@gmail.com>
2021-09-16 18:59:00 +0200

    Task WDP210901-36

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './ProductBox.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExchangeAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
+
 import Button from '../Button/Button';
 import ProductRating from '../../features/ProductRating/ProductRatingContainer';
+import ProductPopup from '../../features/ProductPopup/ProductPopup';
 
 const ProductBox = ({
   id,
@@ -16,67 +18,92 @@ const ProductBox = ({
   image,
   stars,
   oldPrice,
+  category,
+  description,
   favourite,
   addToFavourite,
   isStarred,
   compare,
   addToCompare,
   numberOfProductsToCompare,
-}) => (
-  <div className={styles.root}>
-    <div className={styles.photo}>
-      <img src={image} alt={name} />
-      {promo && <div className={styles.sale}>{promo}</div>}
-      <div className={styles.buttons}>
-        <Button variant='small'>Quick View</Button>
-        <Button variant='small'>
-          <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
-        </Button>
-      </div>
-    </div>
-    <div className={styles.content}>
-      <h5>{name}</h5>
-      <div className={styles.stars}>
-        <ProductRating id={id} stars={stars} isStarred={isStarred} />
-      </div>
-    </div>
-    <div className={styles.line}></div>
-    <div className={styles.actions}>
-      <div className={styles.outlines}>
-        <Button
-          variant={favourite ? 'favourite' : 'outline'}
-          onClick={event => {
-            event.preventDefault();
-            return addToFavourite(id);
-          }}
-        >
-          <FontAwesomeIcon icon={faHeart}>Favourite</FontAwesomeIcon>
-        </Button>
-        <Button
-          variant={compare ? 'compare' : 'outline'}
-          onClick={event => {
-            if (numberOfProductsToCompare >= 4) {
-              event.preventDefault();
-              alert('You can only add four products to compare!');
-            } else {
-              event.preventDefault();
-              return addToCompare(id);
-            }
-          }}
-        >
-          <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
-        </Button>
-      </div>
-      <div className={styles.price}>
-        <div className={styles.oldPrice}>{oldPrice ? '$ ' + oldPrice : ''}</div>
-        <Button noHover variant='small'>
-          $ {price}
-        </Button>
-      </div>
-    </div>
-  </div>
-);
+}) => {
+  const [showPopup, togglePopup] = useState(false);
 
+  const handlePopup = event => {
+    event.preventDefault();
+    return togglePopup(!showPopup);
+  };
+  return (
+    <div className={styles.root}>
+      <div className={styles.photo}>
+        <img src={image} alt={name} />
+        {promo && <div className={styles.sale}>{promo}</div>}
+        <div className={styles.buttons}>
+          <Button variant={'small'} onClick={event => handlePopup(event)}>
+            QUICK VIEW
+          </Button>
+          <Button variant='small'>
+            <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
+          </Button>
+        </div>
+      </div>
+      {showPopup ? (
+        <ProductPopup
+          id={id}
+          name={name}
+          image={image}
+          price={price}
+          promo={promo}
+          category={category}
+          description={description}
+          closePopup={handlePopup}
+        />
+      ) : (
+        ''
+      )}
+      <div className={styles.content}>
+        <h5>{name}</h5>
+        <div className={styles.stars}>
+          <ProductRating id={id} stars={stars} isStarred={isStarred} />
+        </div>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.actions}>
+        <div className={styles.outlines}>
+          <Button
+            variant={favourite ? 'favourite' : 'outline'}
+            onClick={event => {
+              event.preventDefault();
+              return addToFavourite(id);
+            }}
+          >
+            <FontAwesomeIcon icon={faHeart}>Favourite</FontAwesomeIcon>
+          </Button>
+          <Button
+            variant={compare ? 'compare' : 'outline'}
+            onClick={event => {
+              if (numberOfProductsToCompare >= 4) {
+                event.preventDefault();
+                alert('You can only add four products to compare!');
+              } else {
+                event.preventDefault();
+                return addToCompare(id);
+              }
+            }}
+          >
+            <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
+          </Button>
+        </div>
+        <div className={styles.price}>
+          <div className={styles.oldPrice}>{oldPrice ? '$ ' + oldPrice : ''}</div>
+          <Button noHover variant='small'>
+            $ {price}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 ProductBox.propTypes = {
   id: PropTypes.string,
   children: PropTypes.node,
@@ -85,6 +112,8 @@ ProductBox.propTypes = {
   price: PropTypes.number,
   oldPrice: PropTypes.number,
   promo: PropTypes.string,
+  category: PropTypes.string,
+  description: PropTypes.string,
   favourite: PropTypes.bool,
   compare: PropTypes.bool,
   addToFavourite: PropTypes.func,
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -21,6 +21,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/945688/pexels-photo-945688.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum pharetra metus, id tristique massa pulvinar ut.',
     },
     {
       id: 'aenean-ru-bristique-2',
@@ -35,6 +37,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/276566/pexels-photo-276566.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Duis justo mauris, suscipit vitae elit quis, feugiat vehicula nisi. Quisque eleifend vitae quam eu facilisis.',
     },
     {
       id: 'aenean-ru-bristique-3',
@@ -49,6 +53,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/6758245/pexels-photo-6758245.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Aliquam suscipit, erat non dictum venenatis, metus ante molestie nunc, et vehicula dolor metus id nunc.',
     },
     {
       id: 'aenean-ru-bristique-4',
@@ -63,6 +69,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/7018400/pexels-photo-7018400.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Proin convallis tincidunt leo, vitae imperdiet turpis aliquet vitae. ',
     },
     {
       id: 'aenean-ru-bristique-5',
@@ -77,6 +85,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/2440471/pexels-photo-2440471.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Sed non odio ut magna sollicitudin eleifend nec vel lorem. Quisque vestibulum orci nec facilisis dapibus.',
     },
     {
       id: 'aenean-ru-bristique-6',
@@ -91,6 +101,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/5998138/pexels-photo-5998138.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'In nec velit facilisis velit porttitor varius vitae eget massa. Sed sollicitudin nec velit vel bibendum.',
     },
     {
       id: 'aenean-ru-bristique-7',
@@ -105,6 +117,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
       compare: false,
+      description:
+        'Vestibulum laoreet faucibus commodo. Pellentesque accumsan mi id lectus efficitur, nec dignissim neque tincidunt. ',
     },
     {
       id: 'aenean-ru-bristique-8',
@@ -119,6 +133,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/6538933/pexels-photo-6538933.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'In porttitor finibus laoreet. Ut non purus quis diam condimentum tempor ut sed risus. Praesent condimentum congue mauris. ',
     },
     {
       id: 'aenean-ru-bristique-9',
@@ -133,6 +149,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/6782476/pexels-photo-6782476.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida non augue et dignissim. ',
     },
     {
       id: 'aenean-ru-bristique-10',
@@ -147,6 +165,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/7088273/pexels-photo-7088273.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Integer eu purus diam. Aliquam sagittis arcu odio, at auctor sapien iaculis vel. ',
     },
     {
       id: 'aenean-ru-bristique-11',
@@ -161,6 +181,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/6969824/pexels-photo-6969824.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Praesent consectetur lorem vel justo convallis, in eleifend tellus egestas. ',
     },
     {
       id: 'aenean-ru-bristique-12',
@@ -175,6 +197,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/6588592/pexels-photo-6588592.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Cras commodo magna ut dolor sagittis finibus. Praesent vitae nisi mollis arcu tincidunt lacinia ac ac felis.',
     },
     {
       id: 'aenean-ru-bristique-13',
@@ -189,6 +213,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/6947272/pexels-photo-6947272.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Vivamus ultricies euismod accumsan. Etiam hendrerit pretium eros, porttitor vehicula ligula ultrices in.',
     },
     {
       id: 'aenean-ru-bristique-14',
@@ -203,6 +229,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/3757055/pexels-photo-3757055.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Aliquam pharetra est nunc, vitae feugiat enim gravida et. Donec sit amet eros consectetur, eleifend ipsum vel, tristique lacus. ',
     },
     {
       id: 'aenean-ru-bristique-15',
@@ -217,6 +245,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/4352247/pexels-photo-4352247.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Phasellus et risus vel nibh condimentum pretium in vitae risus. Donec pretium quam mattis purus efficitur, non condimentum leo porttitor.',
     },
     {
       id: 'aenean-ru-bristique-16',
@@ -231,6 +261,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/6969834/pexels-photo-6969834.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Mauris at tincidunt lorem, commodo hendrerit turpis. Proin dignissim odio nisl, nec vulputate tellus interdum a.',
     },
     {
       id: 'aenean-ru-bristique-17',
@@ -245,6 +277,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/775219/pexels-photo-775219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Duis suscipit pharetra gravida. In malesuada nibh at dolor auctor tristique. Aliquam massa risus, viverra sed pharetra ut, dapibus in lacus.',
     },
     {
       id: 'aenean-ru-bristique-18',
@@ -259,6 +293,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/6480198/pexels-photo-6480198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Nunc ac mi ac odio malesuada auctor id non sapien. Etiam in velit bibendum, consequat nulla quis, scelerisque quam. ',
     },
     {
       id: 'aenean-ru-bristique-19',
@@ -273,6 +309,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/1374125/pexels-photo-1374125.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Aliquam consectetur, velit non porta vehicula, massa nunc faucibus nisi, vel lobortis odio nibh quis lacus. Etiam tristique a sapien nec elementum.',
     },
     {
       id: 'aenean-ru-bristique-20',
@@ -287,6 +325,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Cras laoreet, tortor ac rutrum vulputate, odio urna faucibus tortor, in semper diam risus ac nisl.',
     },
     {
       id: 'aenean-ru-bristique-21',
@@ -301,6 +341,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/833045/pexels-photo-833045.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Sed pretium eros ac varius volutpat. Mauris convallis dapibus neque non congue. Suspendisse ac urna et lectus eleifend hendrerit. ',
     },
     {
       id: 'aenean-ru-bristique-22',
@@ -315,6 +357,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/439227/pexels-photo-439227.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Integer hendrerit sollicitudin leo sit amet convallis. Nullam viverra lectus vel vulputate ultricies. Aenean at est nec turpis porta pulvinar.',
     },
     {
       id: 'aenean-ru-bristique-23',
@@ -329,6 +373,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/1648768/pexels-photo-1648768.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Etiam hendrerit pretium eros, porttitor vehicula ligula ultrices in. Aliquam pharetra est nunc, vitae feugiat enim gravida et.',
     },
     {
       id: 'aenean-ru-bristique-24',
@@ -343,6 +389,8 @@ const initialState = {
       image:
         'https://images.pexels.com/photos/276534/pexels-photo-276534.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       compare: false,
+      description:
+        'Vestibulum laoreet faucibus commodo. Pellentesque accumsan mi id lectus efficitur, nec dignissim neque tincidunt. ',
     },
   ],
   cart: {
