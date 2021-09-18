0b8f30ca864e3f032cbe2f324c0d3494a44c6bf1
Agnieszka Falandys <agnfal@gmail.com>
2021-09-18 21:46:42 +0200

    Task WDP210901-47

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './CompanyClaim.module.scss';
-
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMobileAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 
@@ -22,12 +22,14 @@ const CompanyClaim = ({ cartCount }) => (
           </a>
         </div>
         <div className={`col text-right ${styles.cart}`}>
-          <a href='#' className={styles.cartBox}>
-            <div className={styles.cartIcon}>
-              <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
-            </div>
-            <div className={styles.cartCounter}>{cartCount}</div>
-          </a>
+          <NavLink exact to={'/cart'} activeClassName='active'>
+            <a href='#' className={styles.cartBox}>
+              <div className={styles.cartIcon}>
+                <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
+              </div>
+              <div className={styles.cartCounter}>{cartCount}</div>
+            </a>
+          </NavLink>
           <div className={styles.phoneNumber}>
             <p>
               <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560
