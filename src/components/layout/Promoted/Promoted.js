503c74deebb3c79a08c4c93e0ce4f3517635afb4
Marcin Skowronek <marcinfoto35@op.pl>
2021-09-18 14:33:07 +0200

    Animation is working

--- src/components/layout/Promoted/Promoted.js
+++ src/components/layout/Promoted/Promoted.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Swipeable from '../../common/Swipeable/Swipeable';
 import styles from './Promoted.module.scss';
 import PromotedProductBox from '../../common/PromotedProductBox/PromotedProductBoxContainer';
 import Button from '../../common/Button/Button';
 import Fade from 'react-reveal/Fade';
 import Carousel from 'react-material-ui-carousel';
+
+// import reducer from '../../../redux/productsRedux';
 
 class Promoted extends React.Component {
   state = {
@@ -39,19 +40,19 @@ class Promoted extends React.Component {
     ];
 
     /* Slider small actions */
-    const rightAction = () => {
-      const newPage = activePageSmall - 1;
-      if (newPage >= 0) {
-        this.setState({ activePageSmall: newPage });
-      }
-    };
+    // const rightAction = () => {
+    //   const newPage = activePageSmall - 1;
+    //   if (newPage >= 0) {
+    //     this.setState({ activePageSmall: newPage });
+    //   }
+    // };
 
-    const leftAction = () => {
-      const newPage = activePageSmall + 1;
-      if (newPage < pagesCount) {
-        this.setState({ activePageSmall: newPage });
-      }
-    };
+    // const leftAction = () => {
+    //   const newPage = activePageSmall + 1;
+    //   if (newPage < pagesCount) {
+    //     this.setState({ activePageSmall: newPage });
+    //   }
+    // };
 
     const dots = [];
     for (let i = 0; i < pagesCount; i++) {
@@ -72,32 +73,81 @@ class Promoted extends React.Component {
     const rightPage = activePageBig + 1;
 
     return (
-      <Swipeable leftAction={leftAction} rightAction={rightAction}>
-        <section className={styles.root}>
-          <div className='container-lg'>
-            <div className={'row'}>
-              <div
-                className={`col-lg-4 d-none d-lg-block ${styles.promotionWrapperLeft}`}
-              >
-                <div className={styles.panelBar}>
-                  <div className='row justify-content-between'>
-                    <div className={`col-5 ${styles.title}`}>HOT DEALS</div>
-                    <div className={`col-5 ${styles.dots}`}>
-                      <ul>{dots}</ul>
-                    </div>
-                  </div>
+      <section className={styles.root}>
+        <div className='container'>
+          <div className={'row'}>
+            <div className={`col-4 ${styles.promotionWrapperLeft}`}>
+              {/* <Swipeable leftAction={leftAction} rightAction={rightAction}> */}
+              <div className={styles.panelBar}>
+                {/* <div className='row justify-content-center'> */}
+                <div className={`${styles.title}`}>
+                  <h5>HOT DEALS</h5>
                 </div>
-                <Carousel autoPlay='true' interval='2000' animation='fade'>
-                  {promoProducts
-                    .slice(activePageSmall, activePageSmall + 1)
-                    .map(prom => (
-                      <div key={prom.id}>
-                        <Fade>
-                          <PromotedProductBox {...prom} />
-                        </Fade>
-                      </div>
-                    ))}
+                <Carousel
+                  autoPlay={true}
+                  interval='3000'
+                  animation='fade'
+                  navButtonsProps={{
+                    style: {
+                      display: 'none',
+                    },
+                  }}
+                  activeIndicatorIconButtonProps={{
+                    style: {
+                      color: '#D58E32',
+                    },
+                  }}
+                  indicatorIconButtonProps={{
+                    style: {
+                      // display: 'none',
+                      // color: 'red',
+                    },
+                  }}
+                >
+                  {promoProducts.map(prom => (
+                    <PromotedProductBox key={prom.id} {...prom} />
+                  ))}
                 </Carousel>
+                {/* </div> */}
+              </div>
+              {/* <div className={`col-5 ${styles.dots}`}>
+                      <ul>{dots}</ul>
+                    </div> */}
+
+              {/* </div> */}
+
+              {/* {promoProducts
+                  .slice(activePageSmall, activePageSmall + 1)
+                  .map(prom => (
+
+                    <div key={prom.id}>
+                      <Fade>
+                        <PromotedProductBox {...prom} />
+                      </Fade>
+                    </div>
+
+                  ))} */}
+
+              {/* </Swipeable> */}
+            </div>
+
+            <div className={`col-8 ${styles.promotionWrapperRight}`}>
+              {promoProductsTwo.slice(activePageBig, activePageBig + 1).map(prom => (
+                <div key={prom.id} className={styles.imgWrapper}>
+                  <Fade>
+                    <img src={prom.image} alt={123} />
+                  </Fade>
+                </div>
+              ))}
+              <div className={styles.shadowWrapper}></div>
+              <div className={styles.shadowTitle}>
+                INDOOR <span>FURNITURE</span>
+                <div className={styles.shadowSubtitle}>
+                  SAVE UP TO 50% OF ALL FURNITURE
+                </div>
+              </div>
+              <div className={styles.mainButton}>
+                <Button variant='white'>SHOP NOW</Button>
               </div>
               <div className={`col-8 ${styles.promotionWrapperRight}`}>
                 {promoProductsTwo.slice(activePageBig, activePageBig + 1).map(prom => (
@@ -174,8 +224,8 @@ class Promoted extends React.Component {
               </div>
             </div>
           </div>
-        </section>
-      </Swipeable>
+        </div>
+      </section>
     );
   }
 }
