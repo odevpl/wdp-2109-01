6ad4ecf09c3735f580b10c21982fb4ebdff2bdb1
Kamila Kowalczyk-Radomska <kamila_kodilla@o2.pl>
2021-09-19 16:23:18 +0200

    Add NewFurniture

--- src/components/views/Search/Search.js
+++ src/components/views/Search/Search.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './Search.module.scss';
+import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 
-const Search = () => <div className={styles.root}>WITAJ</div>;
+const Search = () => (
+  <div className={styles.root}>
+    WITAJ
+    <NewFurniture />
+  </div>
+);
 
 export default Search;
