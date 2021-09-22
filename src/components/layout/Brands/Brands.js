c903b714f8aae29c4eb549a8325a137d6d04a269
Marcin Skowronek <marcinfoto35@op.pl>
2021-09-22 17:34:05 +0200

    Combination with grid

--- src/components/layout/Brands/Brands.js
+++ src/components/layout/Brands/Brands.js
@@ -5,6 +5,8 @@ import Button from '../../common/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import BrandsBox from '../../features/BrandsBox/BrandsBox';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 const Brands = ({ brands }) => {
   const [brand] = useState(6);
@@ -26,41 +28,46 @@ const Brands = ({ brands }) => {
     <div className={styles.root}>
       <div className={styles.wrapper}>
         <div className='container'>
-          <div
-            className={'row no-gutters justify-content-between ' + styles.sliderWrapper}
-          >
-            <div className={`col-1`}>
-              <Button
-                className={styles.button}
-                variant='bigCarousel'
-                onClick={handleClickPrevious}
-              >
-                <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
-              </Button>
-            </div>
-
-            <div className='col-10'>
-              <div className='row no-gutters justify-content-between'>
+          <Container className={styles.container}>
+            <Grid container spacing={1} className={styles.grid}>
+              {/* <div className='row no-gutters justify-content-center'> */}
+              {/* <div className={`col-1`}> */}
+              <Grid item xs={1} md={1} lg={1}>
+                <Button
+                  className={styles.button}
+                  variant='bigCarousel'
+                  onClick={handleClickPrevious}
+                >
+                  <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
+                </Button>
+                {/* </div> */}
+              </Grid>
+              {/* <div className='col-10'> */}
+              <Grid container lg={10} direction='row'>
                 {brands
                   .slice(activePage * brand, (activePage + 1) * brand)
                   .map(item => (
-                    <div key={item.id} className='col-2'>
+                    <Grid item key={item.id}>
                       <BrandsBox {...item} />
-                    </div>
+                    </Grid>
                   ))}
-              </div>
-            </div>
+              </Grid>
+              {/* </div> */}
 
-            <div className={`col-1`}>
-              <Button
-                className={styles.button}
-                variant='bigCarousel'
-                onClick={handleClickNext}
-              >
-                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
-              </Button>
-            </div>
-          </div>
+              {/* <div className={`col-1`}> */}
+              <Grid item xs={1} md={1} lg={1}>
+                <Button
+                  className={styles.button}
+                  variant='bigCarousel'
+                  onClick={handleClickNext}
+                >
+                  <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+                </Button>
+                {/* </div> */}
+              </Grid>
+              {/* </div> */}
+            </Grid>
+          </Container>
         </div>
       </div>
     </div>
