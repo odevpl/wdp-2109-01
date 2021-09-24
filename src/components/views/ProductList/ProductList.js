934e982757b29438ebfe97d3db59bd8a37693ea8
Kamila Kowalczyk-Radomska <kamila_kodilla@o2.pl>
2021-09-24 22:51:52 +0200

    Add filter by categories

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -29,7 +29,63 @@ const ProductList = () => (
         <div className='col-9'>
           <ShopFurniture tileNumber={12} />
         </div>
-        <div className='col-3'>Filters</div>
+        <div className='col-3'>
+          <div className={styles.byCategories}>
+            <h4>FILTER BY CATEGORIES</h4>
+            <div className='row'>
+              <Button className={styles.buttonActive}>
+                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+              </Button>
+              <div className={styles.activeCategory}>Furniture</div>
+              <div className={styles.circleActive}>3</div>
+            </div>
+            <div className='row'>
+              <Button className={styles.button}>
+                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+              </Button>
+              <div className={styles.category}>
+                <h6>Sofa</h6>
+              </div>
+              <div className={styles.circle}>4</div>
+            </div>
+            <div className='row'>
+              <Button className={styles.button}>
+                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+              </Button>
+              <div className={styles.category}>
+                <h6>Chair</h6>
+              </div>
+              <div className={styles.circle}>5</div>
+            </div>
+            <div className='row'>
+              <Button className={styles.button}>
+                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+              </Button>
+              <div className={styles.category}>
+                <h6>Table</h6>
+              </div>
+              <div className={styles.circle}>5</div>
+            </div>
+            <div className='row'>
+              <Button className={styles.button}>
+                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+              </Button>
+              <div className={styles.category}>
+                <h6>Bedroom</h6>
+              </div>
+              <div className={styles.circle}>5</div>
+            </div>
+            <div className='row'>
+              <Button className={styles.button}>
+                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+              </Button>
+              <div className={styles.category}>
+                <h6>Furniture</h6>
+              </div>
+              <div className={styles.circle}>5</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <div className='container'>
