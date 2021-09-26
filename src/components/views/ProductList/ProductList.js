d73e43616b53caab53612fb3cea351ce04e1c8c5
Kamila Kowalczyk-Radomska <kamila_kodilla@o2.pl>
2021-09-26 22:05:10 +0200

    Add component for filters by categories

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -6,6 +6,9 @@ import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import Brands from '../../layout/Brands/BrandsContainer';
 import ShopFurniture from '../../features/ShopFurniture/ShopFurnitureContainer';
 import ProductBanner from '../../common/images/productListBanner.png';
+import Brands from '../../layout/Brands/BrandsContainer';
+import ShopFurniture from '../../features/ShopFurniture/ShopFurnitureContainer';
+import FilterByCategories from '../../features/FilterByCategories/FilterByCategories';
 
 const ProductList = () => (
   <div className={styles.root}>
@@ -30,61 +33,7 @@ const ProductList = () => (
           <ShopFurniture tileNumber={12} />
         </div>
         <div className='col-3'>
-          <div className={styles.byCategories}>
-            <h4>FILTER BY CATEGORIES</h4>
-            <div className='row'>
-              <Button className={styles.buttonActive}>
-                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
-              </Button>
-              <div className={styles.activeCategory}>Furniture</div>
-              <div className={styles.circleActive}>3</div>
-            </div>
-            <div className='row'>
-              <Button className={styles.button}>
-                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
-              </Button>
-              <div className={styles.category}>
-                <h6>Sofa</h6>
-              </div>
-              <div className={styles.circle}>4</div>
-            </div>
-            <div className='row'>
-              <Button className={styles.button}>
-                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
-              </Button>
-              <div className={styles.category}>
-                <h6>Chair</h6>
-              </div>
-              <div className={styles.circle}>5</div>
-            </div>
-            <div className='row'>
-              <Button className={styles.button}>
-                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
-              </Button>
-              <div className={styles.category}>
-                <h6>Table</h6>
-              </div>
-              <div className={styles.circle}>5</div>
-            </div>
-            <div className='row'>
-              <Button className={styles.button}>
-                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
-              </Button>
-              <div className={styles.category}>
-                <h6>Bedroom</h6>
-              </div>
-              <div className={styles.circle}>5</div>
-            </div>
-            <div className='row'>
-              <Button className={styles.button}>
-                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
-              </Button>
-              <div className={styles.category}>
-                <h6>Furniture</h6>
-              </div>
-              <div className={styles.circle}>5</div>
-            </div>
-          </div>
+          <FilterByCategories />
         </div>
       </div>
     </div>
