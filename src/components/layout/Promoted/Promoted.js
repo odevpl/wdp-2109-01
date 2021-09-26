a73eadcb81a6e6906e85ce66d21c206a01321a3e
Marcin Skowronek <marcinfoto35@op.pl>
2021-09-26 20:42:34 +0200

    Repair Promoted component after rebase

--- src/components/layout/Promoted/Promoted.js
+++ src/components/layout/Promoted/Promoted.js
@@ -5,7 +5,9 @@ import PromotedProductBox from '../../common/PromotedProductBox/PromotedProductB
 import Button from '../../common/Button/Button';
 import Fade from 'react-reveal/Fade';
 import Carousel from 'react-material-ui-carousel';
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Swipeable from '../../common/Swipeable/Swipeable';
+
+// import reducer from '../../../redux/productsRedux';
 
 class Promoted extends React.Component {
   state = {
@@ -44,6 +46,20 @@ class Promoted extends React.Component {
     ];
 
     /* Slider small actions */
+    // const rightActionCarousel = () => {
+    //   const newPage = activePageSmall - 1;
+    //   if (newPage >= 0) {
+    //     this.setState({ activePageSmall: newPage });
+    //   }
+    // };
+
+    // const leftActionCarousel = () => {
+    //   const newPage = activePageSmall + 1;
+    //   if (newPage < pagesCount) {
+    //     this.setState({ activePageSmall: newPage });
+    //   }
+    // };
+
     const rightAction = () => {
       const newPage = activePage - 1;
       if (newPage >= 0) {
@@ -77,181 +93,107 @@ class Promoted extends React.Component {
     const rightPage = activePageBig + 1;
 
     return (
-      // <Swipeable leftAction={leftAction} rightAction={rightAction}>
-      <section className={styles.root}>
-        <div className='container'>
-          <div className='row'>
-            <div
-              className={`d-none d-md-block col-12 col-lg-4 ${styles.promotionWrapperLeft}`}
-            >
-              <div className={styles.panelBar}>
-                {/* <div className='row justify-content-center'> */}
-                {/* <div className={`${styles.title}`}>
-                  <h5>HOT DEALS</h5> */}
-                {/* <div className={`${styles.dots}`}>
-                    <ul>{dots}</ul>
-                  </div> */}
-                {/* <div className="Carousel-indicators-2">
-                    <button className="MuiButtonBase-root MuiIconButton-root Carousel-indicator-3
-                      MuiIconButton-sizeSmall" tabIndex="0" type="button" aria-label="carousel indicator 1" style={{ color: '#e1e1e1' }}>
-                      <span className="MuiIconButton-label">
-                        <svg className="MuiSvgIcon-root Carousel-indicatorIcon-4"
-                          focusable="false" viewBox="0 0 24 24" aria-hidden="true" style={{
-                            fontSize: 20,
-                          }}>
-                          <circle cx="12" cy="12" r="8"></circle>
-                        </svg>
-                      </span>
-                      <span className="MuiTouchRipple-root">
-                      </span>
-                    </button>
-                    <button className="MuiButtonBase-root MuiIconButton-root Carousel-indicator-3  Carousel-active-5  MuiIconButton-sizeSmall"
-                      tabIndex="0" type="button" aria-label="carousel indicator 2" style={{
+      <Swipeable leftAction={leftAction} rightAction={rightAction}>
+        <section className={styles.root}>
+          <div className='container'>
+            <div className={'row'}>
+              <div className={`col-4 ${styles.promotionWrapperLeft}`}>
+                <div className={styles.panelBar}>
+                  <Carousel
+                    classes={{ indicatorIcon: 'MyCustomButton' }}
+                    autoPlay={true}
+                    interval='3000'
+                    animation='fade'
+                    navButtonsProps={{
+                      style: {
+                        display: 'none',
+                        fontSize: 20,
+                      },
+                    }}
+                    activeIndicatorIconButtonProps={{
+                      style: {
                         color: '#D58E32',
-                        fontSize: 18,
-                      }}>
-                      <span className="MuiIconButton-label">
-                        <svg className="MuiSvgIcon-root Carousel-indicatorIcon-4"
-                          focusable="false" viewBox="0 0 24 24" aria-hidden="true" style={{
-                            fontSize: 20,
-                          }}>
-                          <circle cx="12" cy="12" r="8">
-                          </circle>
-                        </svg>
-                      </span>
-                      <span
-                        className="MuiTouchRipple-root">
-                      </span>
-                    </button>
-                    <button
-                      className="MuiButtonBase-root MuiIconButton-root Carousel-indicator-3  MuiIconButton-sizeSmall"
-                      tabIndex="0" type="button" aria-label="carousel indicator 3" style={{ color: '#e1e1e1' }}>
-                      <span className="MuiIconButton-label">
-                        <svg className="MuiSvgIcon-root Carousel-indicatorIcon-4" focusable="false" viewBox="0 0 24 24"
-                          aria-hidden="true" style={{
-                            fontSize: 20,
-                          }}>
-                          <circle cx="12" cy="12" r="8">
-                          </circle>
-                        </svg>
-                      </span>
-                      <span className="MuiTouchRipple-root">
-                      </span>
-                    </button>
-                  </div> */}
-                {/* </div> */}
-                {/* <ThemeProvider theme={theme}> */}
-                <Carousel
-                  sx={{
-                    color: 'red',
-                    '& .Carousel-indicatorIcon-4': {
-                      display: 'none',
-                    },
-                  }}
-                  autoPlay={true}
-                  interval='3000'
-                  animation='fade'
-                  navButtonsProps={{
-                    style: {
-                      display: 'none',
-                      fontSize: 20,
-                    },
-                  }}
-                  activeIndicatorIconButtonProps={{
-                    style: {
-                      color: '#D58E32',
-                      fontSize: 20,
-                    },
-                  }}
-                  indicatorIconButtonProps={{
-                    indicatorIcon: {
-                      fontSize: '20px',
-                    },
-                    style: {
-                      color: '#e1e1e1',
-                      fontSize: 20,
+                      },
+                    }}
+                    indicatorIconButtonProps={{
                       '& .hover': {
                         color: '#D58E32',
                       },
-                    },
-                  }}
-                  indicatorContainerProps={{
-                    style: {
-                      position: 'absolute',
-                      top: -2,
-                      textAlign: 'right',
-                      paddingRight: 20,
-                      fontSize: 20,
-                    },
-                  }}
-                >
-                  {promoProducts.map(prom => (
-                    <PromotedProductBox key={prom.id} {...prom} />
-                  ))}
-                </Carousel>
-                {/* </ThemeProvider> */}
-                {/* </div> */}
+                      indicatorIcon: {
+                        fontSize: '20px',
+                      },
+                      style: {
+                        color: '#e1e1e1',
+                        fontSize: 20,
+                        '& .hover': {
+                          color: '#D58E32',
+                        },
+                      },
+                    }}
+                    indicatorContainerProps={{
+                      style: {
+                        position: 'absolute',
+                        top: -2,
+                        textAlign: 'right',
+                        paddingRight: 20,
+                        fontSize: 20,
+                      },
+                    }}
+                  >
+                    {promoProducts.map(prom => (
+                      <PromotedProductBox key={prom.id} {...prom} />
+                    ))}
+                  </Carousel>
+                </div>
               </div>
-              <Carousel autoPlay='true' interval='2000' animation='fade'>
-                {promoProducts
-                  .slice(activePageSmall, activePageSmall + 1)
-                  .map(prom => (
-                    <div key={prom.id}>
-                      <Fade>
-                        <PromotedProductBox {...prom} />
-                      </Fade>
-                    </div>
-                  ))}
-              </Carousel>
-            </div>
 
-            <div className={`col-8 ${styles.promotionWrapperRight}`}>
-              {promoProductsTwo.slice(activePageBig, activePageBig + 1).map(prom => (
-                <div key={prom.id} className={styles.imgWrapper}>
-                  <Fade>
-                    <img src={prom.image} alt={123} />
-                  </Fade>
-                </div>
-              ))}
-              <div className={styles.shadowWrapper}></div>
-              <div className={styles.shadowTitle}>
-                INDOOR <span>FURNITURE</span>
-                <div className={styles.shadowSubtitle}>
-                  SAVE UP TO 50% OF ALL FURNITURE
-                </div>
-              </div>
-              <div className={styles.mainButton}>
-                <Button variant='white'>SHOP NOW</Button>
-              </div>
-              <div className={styles.buttonsWrapper}>
-                <div className='row'>
-                  <div className={`col-6 ${styles.button}`}>
-                    <Button
-                      variant='long'
-                      onClick={e =>
-                        e.preventDefault() & this.handlePageChangeSliderBG(leftPage)
-                      }
-                    >
-                      {'<'}
-                    </Button>
+              <div className={`col-8 ${styles.promotionWrapperRight}`}>
+                {promoProductsTwo.slice(activePageBig, activePageBig + 1).map(prom => (
+                  <div key={prom.id} className={styles.imgWrapper}>
+                    <Fade>
+                      <img src={prom.image} alt={123} />
+                    </Fade>
                   </div>
-                  <div className={`col-6 ${styles.button}`}>
-                    <Button
-                      variant='long'
-                      onClick={e =>
-                        e.preventDefault() & this.handlePageChangeSliderBG(rightPage)
-                      }
-                    >
-                      {'>'}
-                    </Button>
+                ))}
+                <div className={styles.shadowWrapper}></div>
+                <div className={styles.shadowTitle}>
+                  INDOOR <span>FURNITURE</span>
+                  <div className={styles.shadowSubtitle}>
+                    SAVE UP TO 50% OF ALL FURNITURE
+                  </div>
+                </div>
+                <div className={styles.mainButton}>
+                  <Button variant='white'>SHOP NOW</Button>
+                </div>
+                <div className={styles.buttonsWrapper}>
+                  <div className='row'>
+                    <div className={`col-6 ${styles.button}`}>
+                      <Button
+                        variant='long'
+                        onClick={e =>
+                          e.preventDefault() & this.handlePageChangeSliderBG(leftPage)
+                        }
+                      >
+                        {'<'}
+                      </Button>
+                    </div>
+                    <div className={`col-6 ${styles.button}`}>
+                      <Button
+                        variant='long'
+                        onClick={e =>
+                          e.preventDefault() & this.handlePageChangeSliderBG(rightPage)
+                        }
+                      >
+                        {'>'}
+                      </Button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      // </Swipeable>
+        </section>
+      </Swipeable>
     );
   }
 }
