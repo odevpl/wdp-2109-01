125f5573d7ae2e6ac472c884d7ce12b7d20dab50
Kai Mareving <karska.joanna@gmail.com>
2021-09-28 06:03:41 +0200

    2nd tier lists with styling

--- src/components/layout/TopBar/TopBar.js
+++ src/components/layout/TopBar/TopBar.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faUser, faLock, faBars } from '@fortawesome/free-solid-svg-icons';
@@ -6,77 +7,99 @@ import { faCaretDown, faUser, faLock, faBars } from '@fortawesome/free-solid-svg
 import styles from './TopBar.module.scss';
 import { Link, NavLink } from 'react-router-dom';
 
-const TopBar = () => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row'>
-        <div className={`col text-left ${styles.topOptions}`}>
-          <ul>
-            <li>
-              <button type='button'>
-                USD <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-              </button>
-              <ul className={styles.secondaryList}>
-                <li>
-                  <button type='button'>
-                    PLN <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-                  </button>
-                </li>
-                <li>
-                  <button type='button'>
-                    EUR <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-                  </button>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <button type='button'>
-                English <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-              </button>
-              <ul className={styles.secondaryList}>
-                <li>
-                  <button type='button'>
-                    Polish{' '}
-                    <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-                  </button>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <button type='button'>
-                Help <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-              </button>
-            </li>
-          </ul>
-        </div>
-        <div className={`col text-right ${styles.topMenu}`}>
-          <ul>
-            <li>
-              <NavLink exact to={'/login'} activeClassName='active'>
-                <button type='button'>
-                  <FontAwesomeIcon className={styles.icon} icon={faUser} /> Login
-                </button>
-              </NavLink>
-            </li>
-            <li>
-              <button type='button'>
-                <Link to='/register'>
-                  <FontAwesomeIcon className={styles.icon} icon={faLock} />
-                  Register
-                </Link>
-              </button>
-            </li>
+const TopBar = () => {
+  const [language, setLanguage] = useState('English');
+  const [currency, setCurrency] = useState('USD');
 
-            <li>
-              <button type='button'>
-                <FontAwesomeIcon className={styles.icon} icon={faBars} />
-              </button>
-            </li>
-          </ul>
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='row'>
+          <div className={`col text-left ${styles.topOptions}`}>
+            <ul>
+              <li>
+                <button type='button'>
+                  Currency{' '}
+                  <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+                </button>
+                <ul className={styles.secondaryList}>
+                  <li>
+                    <button type='button' onClick={() => setCurrency('USD')}>
+                      USD
+                    </button>
+                  </li>
+                  <li>
+                    <button type='button' onClick={() => setCurrency('PLN')}>
+                      PLN
+                    </button>
+                  </li>
+                  <li>
+                    <button type='button' onClick={() => setCurrency('EUR')}>
+                      EUR
+                    </button>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <button type='button'>
+                  Language{' '}
+                  <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+                </button>
+                <ul className={styles.secondaryList}>
+                  <li>
+                    <button type='button' onClick={() => setLanguage('English')}>
+                      English
+                    </button>
+                  </li>
+                  <li>
+                    <button type='button' onClick={() => setLanguage('Polish')}>
+                      Polish
+                    </button>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <button type='button'>
+                  Help <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+                </button>
+              </li>
+            </ul>
+          </div>
+          <div className={`col text-right ${styles.topMenu}`}>
+            <ul>
+              <li>
+                <NavLink exact to={'/login'} activeClassName='active'>
+                  <button type='button'>
+                    <FontAwesomeIcon className={styles.icon} icon={faUser} /> Login
+                  </button>
+                </NavLink>
+              </li>
+              <li>
+                <button type='button'>
+                  <Link to='/register'>
+                    <FontAwesomeIcon className={styles.icon} icon={faLock} />
+                    Register
+                  </Link>
+                </button>
+              </li>
+              <li>
+                <button type='button'>
+                  <FontAwesomeIcon className={styles.icon} icon={faBars} />
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+TopBar.propTypes = {
+  language: PropTypes.string,
+  currency: PropTypes.string,
+  setLanguage: PropTypes.func,
+  setCurrency: PropTypes.func,
+};
 
 export default TopBar;
